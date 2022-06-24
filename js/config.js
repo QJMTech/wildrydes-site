702aaf9d92078363c5ada83b6288d437c5f9626a
Quinn Melssen <77873536+QJMTech@users.noreply.github.com>
2022-06-24 12:26:13 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_EBjgy3Uq2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7b94hukds0kdq4tgi7hl25k8ef', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
